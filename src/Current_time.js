3e114c04b36b87a4f201e2a3fa6a2c8aad6482fa
chaechef <chaeefe@gmail.com>
2020-01-02 14:01:27 +0900

    add current_time

---
+++ src/Current_time.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Current_time.css'
+function current_time() {
+  return (
+    <div className="Current_time">
+        <h1>2020. 01. 02 19:42:45</h1>
+    </div>
+  );
+}
+
+export default current_time;
