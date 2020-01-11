f211cf68519a74f6f62a4d3b1ea2eb8fd2fbc426
chaehoonki <chaeefe@gmail.com>
2020-01-11 14:43:24 -0500

    split timer code

--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import './styles.css'
+import {NOW} from './DateFormat.js'
 
 const CurrentTime = () => {
   const [date, dateSet] = useState(new Date())
@@ -7,13 +8,14 @@ const CurrentTime = () => {
   const tick = () => {
     dateSet(new Date())
   }
+  
   useEffect(()=> {
     setTimeout(tick, 1000)
   },[date])
 
   return (
       <div className="current-time">
-        <h1>{date.toISOString()}</h1>
+        <h1>{NOW(date)}</h1>
       </div>
   );
 }
