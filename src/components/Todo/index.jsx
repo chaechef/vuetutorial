89d3d7434175768c8ad6265153c1ed22fb54b637
chaehoonki <chaeefe@gmail.com>
2020-01-26 23:12:14 -0500

    feat: change style todo component

--- src/components/Todo/index.jsx
+++ src/components/Todo/index.jsx
@@ -1,55 +1,65 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import './style.css';
-import axios from 'axios'
-import {TodoContext} from '../../App.jsx'
-import dayjs from 'dayjs'
+import axios from 'axios';
+import { TodoContext } from '../../App.jsx';
+import dayjs from 'dayjs';
 
-const Todo = ({todo, updateTodo}) => {
+const Todo = ({ todo, updateTodo }) => {
+  const { todos, setTodos, editToggle } = useContext(TodoContext);
 
-  const {todos, setTodos, editToggle} = useContext(TodoContext)
-
-  const updateDay = (todo) => {
-    axios.put('http://localhost:8000/todos/'+todo.id+'/', {
-      ...todo,
-      sum : todo.sum + todo.unit,
-      updateDate: new Date()
-    })
-    .then( res => {
-      updateTodo(res.data)
-    })
-  }
-  const deleteTodo = (todo) => {
-    axios.delete('http://localhost:8000/todos/'+todo.id+'/')
-    .then( res => {
-      const newTodos = [...todos]
-      const index = newTodos.findIndex(v => v.id === todo.id)
-      newTodos.splice(index,1)
-      setTodos(newTodos)
-    })
-  }
+  const updateDay = todo => {
+    axios
+      .put('http://localhost:8000/todos/' + todo.id + '/', {
+        ...todo,
+        sum: todo.sum + todo.unit,
+        updateDate: new Date()
+      })
+      .then(res => {
+        updateTodo(res.data);
+      });
+  };
+  const deleteTodo = todo => {
+    axios.delete('http://localhost:8000/todos/' + todo.id + '/').then(res => {
+      const newTodos = [...todos];
+      const index = newTodos.findIndex(v => v.id === todo.id);
+      newTodos.splice(index, 1);
+      setTodos(newTodos);
+    });
+  };
 
   return (
-      <div className="todo">
-        <div className="stackTime">
-          sum : {todo.sum}
-        </div>
-        <div className="plan">
-          plan : {todo.name}
-          <br/>
-          one day : {todo.unit}
-
-        </div>
-        <div className="todo-footer">
-          {/* <button className="submitBtn"  onClick={(e) => { deleteTodo(todo) }}>삭제</button> */}
-          {dayjs(todo.updateDate).format('YYYY MM-DD') === dayjs(new Date()).format('YYYY MM-DD') ? (
-            <div></div>
-          ) :(
-            <button className="submitBtn"  onClick={(e) => { updateDay(todo) }}>submit</button>
-          ) }
-
-        </div>
+    <div className="todo">
+      <div className="stackTime">sum : {todo.sum}</div>
+      <div className="plan">
+        plan : {todo.name}
+        <br />
+        one day : {todo.unit}
       </div>
+      <div className="todo-footer">
+        <button
+          className="submitBtn"
+          onClick={e => {
+            deleteTodo(todo);
+          }}
+        >
+          delete
+        </button>
+        {dayjs(todo.updateDate).format('YYYY MM-DD') ===
+        dayjs(new Date()).format('YYYY MM-DD') ? (
+          <div></div>
+        ) : (
+          <button
+            className="submitBtn"
+            onClick={e => {
+              updateDay(todo);
+            }}
+          >
+            submit
+          </button>
+        )}
+      </div>
+    </div>
   );
-}
+};
 
 export default Todo;
--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,34 +1,30 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import './styles.css';
 import Todo from '../Todo';
-import {TodoContext} from '../../App.jsx';
+import TodoBeta from '../TodoBeta';
+import { TodoContext } from '../../App.jsx';
 
 const TodoList = () => {
+  const { todos, setTodos } = useContext(TodoContext);
 
-  const {todos, setTodos} = useContext(TodoContext)
-
-  const updateTodo = (newTodo) =>{
-    const newTodos = [...todos]
-    const index = newTodos.findIndex(todo => todo.id === newTodo.id)
-    newTodos[index] = newTodo
-    setTodos(newTodos)
-  }
+  const updateTodo = newTodo => {
+    const newTodos = [...todos];
+    const index = newTodos.findIndex(todo => todo.id === newTodo.id);
+    newTodos[index] = newTodo;
+    setTodos(newTodos);
+  };
 
   return (
     <div className="todoList">
-      {
-        
-        todos.length === 0 ? (
-          <div>No Data</div>
-          ) : (  
-          todos.map( todo => {
-          return <Todo key={todo.id} todo={todo} updateTodo={updateTodo} />
-        }))
-      }
-
-            
+      {todos.length === 0 ? (
+        <div>No Data</div>
+      ) : (
+        todos.map(todo => {
+          return <TodoBeta key={todo.id} todo={todo} updateTodo={updateTodo} />;
+        })
+      )}
     </div>
   );
-}
+};
 
 export default TodoList;
