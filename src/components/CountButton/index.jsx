064cb4172ddddc963da41b6de5d4a4f8a28beb48
chaehoonki <chaeefe@gmail.com>
2020-01-30 16:28:42 -0500

    feat: count button

--- src/components/CountButton/index.jsx
+++ src/components/CountButton/index.jsx
@@ -1,19 +1,22 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 
 const CountButton = () => {
-
   const [count, setCount] = useState(0);
   return (
-      <div className="count">
-            {/* <button className="StartBtn" type="button" onClick={() => setEditToggle(!editToggle)}>
+    <div className="count">
+      {/* <button className="StartBtn" type="button" onClick={() => setEditToggle(!editToggle)}>
                {editToggle ? "done" : "setting"}
             </button> */}
-            <button className="count-button" type="button" onClick={() => setCount(count+1)}>
-               {count}
-            </button>
-      </div>
+      <button
+        className="count-button"
+        type="button"
+        onClick={() => setCount(count + 1)}
+      >
+        {count}
+      </button>
+    </div>
   );
-}
+};
 
 export default CountButton;
