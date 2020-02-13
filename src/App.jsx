5052e9d1bf738e14713cf83366f801d5a1f2e19d
chaehoonki <chaeefe@gmail.com>
2020-02-12 23:04:25 -0500

    feat : add scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,53 @@
-import React, {useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import CurrentTime from './components/Timer';
 import CountButton from './components/CountButton';
 import TodoList from './components/TodoList';
 import PlusButton from './components/PlusButton';
 import CreateForm from './components/CreateForm';
 import axios from 'axios';
-
+import './App.scss';
 export const TodoContext = React.createContext();
 export const ToggleContext = React.createContext();
 export const TimerContext = React.createContext();
 
 const App = () => {
-
   const [todos, setTodos] = useState([]);
   const [isToggleOn, setisToggleOn] = useState(false);
   const [editToggle, setEditToggle] = useState(false);
 
-  const [plan, planSet] = useState("")
-  const [unit, unitSet] = useState("")
+  const [plan, planSet] = useState('');
+  const [unit, unitSet] = useState('');
 
   const fetchTodo = () => {
-    axios.get('http://localhost:8000/todos/')
-    .then(res => {
-      setTodos(res.data)
-    })
-  }
+    axios.get('http://localhost:8000/todos/').then(res => {
+      setTodos(res.data);
+    });
+  };
 
-  useEffect( () => {
-    fetchTodo()
-  }, [])
+  useEffect(() => {
+    fetchTodo();
+  }, []);
 
   return (
-    <TodoContext.Provider value={{todos, setTodos, editToggle, setEditToggle}}>
-        <header className="header">
-            <CurrentTime />
-            <CountButton />
-            <br></br>
-        </header>
-        <section className="main">
-            <TodoList/>
-            <ToggleContext.Provider value={{isToggleOn, setisToggleOn, plan, planSet, unit, unitSet}}>
-              <PlusButton/>
-              <CreateForm/>
-            </ToggleContext.Provider>
-        </section>      
+    <TodoContext.Provider
+      value={{ todos, setTodos, editToggle, setEditToggle }}
+    >
+      <header className="header">
+        <CurrentTime />
+        <CountButton />
+        <br></br>
+      </header>
+      <section className="main">
+        <TodoList />
+        <ToggleContext.Provider
+          value={{ isToggleOn, setisToggleOn, plan, planSet, unit, unitSet }}
+        >
+          <PlusButton />
+          <CreateForm />
+        </ToggleContext.Provider>
+      </section>
     </TodoContext.Provider>
   );
-}
+};
 
 export default App;
